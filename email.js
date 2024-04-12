ff4c1064d0ed1fab5b1847eaaebbb75ea6acd91d
Maheedhar_Windows_machine <Maheedhar@Windows>
2024-04-11 21:49:49 -0400

    update frontend code

--- email.js
+++ email.js
@@ -5,7 +5,7 @@ function showhideEmail() {
     document.getElementById('email').innerHTML = "Show my email";
     shown = false;
   } else {
-    var myemail = "<a href='mailto:sohan" + "@" + "mail.uc.edu'>bodapass" + "@" + "mail.uc.edu</a>";
+    var myemail = "<a href='mailto:sohan" + "@" + "mail.uc.edu'>atmakuma" + "@" + "mail.uc.edu</a>";
     document.getElementById('email').innerHTML = myemail;
     shown = true;
   }
